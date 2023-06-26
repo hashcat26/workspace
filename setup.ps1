b02b8c6df5ac19b753fe20584aa46d83c5ab4365
hashcat26 <dowaitodoratore@gmail.com>
2023-06-26 16:14:02 +0800

    update scripts

--- setup.ps1
+++ setup.ps1
@@ -6,7 +6,7 @@ $PackagesDir = "$PSScriptRoot\packages"
 $GlobalsDir = "$PSScriptRoot\packages\globals"
 Set-Location -LiteralPath $PSScriptRoot
 
-$ScoopFile = "$PSScriptRoot\downloads\scoop.ps1"
+$ScoopFile = "downloads\scoop.ps1"
 $AppList = "cmder", "ffmpeg", "git", "python" ,"vscode"
 $PackageList = "gallery-dl", "spotdl", "yt-dlp"
 
@@ -14,18 +14,18 @@ New-Item downloads, packages, utilities -ItemType Directory
 Invoke-RestMethod get.scoop.sh -OutFile $ScoopFile
 .$ScoopFile -ScoopDir $PackagesDir -ScoopGlobalDir $GlobalsDir
 
-scoop.exe bucket add hashcat https://github.com/hashcat26/bucket
-scoop.exe bucket add main ; scoop.exe bucket add extras
-scoop.exe update ; scoop.exe status
+Invoke-Expression "scoop bucket add extras"
+Invoke-Expression "scoop update"
+Invoke-Expression "scoop status"
 
 foreach ($AppName in $AppList) {
-    scoop.exe install $AppName
+    Invoke-Expression "scoop install $AppName"
 }
 
-python.exe -mod pip install --upgrade pip
-pip.exe install virtualenv ; virtualenv.exe utilities
-. "$PSScriptRoot\utilities\Scripts\activate"
+Invoke-Expression "pip install virtualenv"
+Invoke-Expression "virtualenv utilities"
+Invoke-Expression ". utilities\Scripts\activate"
 
 foreach ($PackageName in $PackageList) {
-    pip.exe install $PackageName
+    Invoke-Expression "pip install $PackageName"
 }
