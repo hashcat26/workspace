1ca17b2dfbec2cb5756e182ce9f026f5430f7f36
hashcat26 <dowaitodoratore@gmail.com>
2023-06-26 12:45:09 +0800

    update scripts

--- setup.ps1
+++ setup.ps1
@@ -2,22 +2,30 @@ $ErrorActionPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+$PackagesDir = "$PSScriptRoot\packages"
 $GlobalsDir = "$PSScriptRoot\packages\globals"
-$ScoopFile = "$PSScriptRoot\downloads\scoop.ps1"
 Set-Location -LiteralPath $PSScriptRoot
+
+$ScoopFile = "$PSScriptRoot\downloads\scoop.ps1"
+$AppList = "cmder", "ffmpeg", "git", "python" ,"vscode"
+$PackageList = "gallery-dl", "spotdl", "yt-dlp"
 
 New-Item downloads, packages, utilities -ItemType Directory
 Invoke-RestMethod get.scoop.sh -OutFile $ScoopFile
-.$ScoopFile -ScoopDir $PSScriptRoot -ScoopGlobalDir $GlobalsDir
+.$ScoopFile -ScoopDir $PackagesDir -ScoopGlobalDir $GlobalsDir
 
-scoop bucket add hashcat https://github.com/hashcat26/bucket
-scoop bucket add main ; scoop bucket add extras
-scoop update ; scoop status
+scoop.exe bucket add hashcat https://github.com/hashcat26/bucket
+scoop.exe bucket add main ; scoop.exe bucket add extras
+scoop.exe update ; scoop.exe status
 
-scoop install 7zip
-scoop install cmder
-scoop install dark
-scoop install ffmpeg
-scoop install git
-scoop install python
-scoop install vscode
+foreach ($AppName in $AppList) {
+    scoop.exe install $AppName
+}
+
+python.exe -mod pip install --upgrade pip
+pip.exe install virtualenv ; virtualenv.exe utilities
+. "$PSScriptRoot\utilities\Scripts\activate"
+
+foreach ($PackageName in $PackageList) {
+    pip.exe install $PackageName
+}
--- update.ps1
+++ update.ps1
@@ -13,3 +13,11 @@ $ConfigFile = "$ConfigsDir\gitconfig"
 Copy-Item $AliasFile "$AppsDir\git\current\etc\profile.d"
 Copy-Item $XmlFile "$AppsDir\cmder\current\vendor\conemu-maximus5"
 Copy-Item $ConfigFile "$AppsDir\git\current\etc"
+
+$KeysFile = "$ConfigsDir\keybindings.json"
+$SettingsFile = "$ConfigsDir\settings.json"
+$TasksFile = "$ConfigsDir\tasks.json"
+
+Copy-Item $KeysFile "$AppsDir\vscode\current\data\user-data\User"
+Copy-Item $SettingsFile "$AppsDir\vscode\current\data\user-data\User"
+Copy-Item $TasksFile "$AppsDir\vscode\current\data\user-data\User"
