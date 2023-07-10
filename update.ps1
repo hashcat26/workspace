bd0fa8bea8fe3e44bc7f6ee2af8f9a8c8c5c3b1a
hashcat26 <dowaitodoratore@gmail.com>
2023-07-10 19:23:36 +0800

    update scripts

--- update.ps1
+++ update.ps1
@@ -6,26 +6,26 @@ $WorkspaceRepo = "https://github.com/hashcat26/workspace.git"
 $ConfigsDir = "$PSScriptRoot\configs"
 Set-Location -LiteralPath $PSScriptRoot
 
-Invoke-Expression "scoop alias add sp 'scoop prefix $args[0]'"
 Invoke-Expression "scoop update ; scoop update -a"
+Invoke-Expression "scoop cache rm -a ; scoop cleanup -a"
 Invoke-Expression "git clone -v $WorkspaceRepo"
 
 $AliasFile = "$ConfigsDir\aliases.sh"
 $XmlFile = "$ConfigsDir\ConEmu.xml"
 $ConfigFile = "$ConfigsDir\gitconfig"
 
-Copy-Item $AliasFile "$(scoop sp 'git')\etc\profile.d"
-Copy-Item $XmlFile "$(scoop sp 'cmder')\vendor\conemu-maximus5"
-Copy-Item $ConfigFile "$(scoop sp 'git')\etc"
+Copy-Item $AliasFile "$(scoop prefix git)\etc\profile.d"
+Copy-Item $XmlFile "$(scoop prefix cmder)\vendor\conemu-maximus5"
+Copy-Item $ConfigFile "$(scoop prefix git)\etc"
 
 $KeyFile = "$ConfigsDir\keybindings.json"
 $JsonFile = "$ConfigsDir\settings.json"
 $TaskFile = "$ConfigsDir\tasks.json"
 
 ForEach ($File In $KeyFile, $JsonFile, $TaskFile) {
-    Copy-Item $File "$(scoop sp 'vscode')\data\user-data\User"
+    Copy-Item $File "$(scoop prefix vscode)\data\user-data\User"
 }
 
 Invoke-Expression "cp.exe -r workspace/* workspace/.* ."
 Invoke-Expression "rm.exe -drf workspace ; attrib +h .git"
-Invoke-Expression "scoop alias rm sp ; git up ; git st"
+Invoke-Expression "git up ; git st ; git lg | head"
