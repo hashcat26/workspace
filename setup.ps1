e6dbd7878c5ae5e5c27c46d8d6ef588a4b2b1dac
hashcat26 <dowaitodoratore@gmail.com>
2023-08-04 22:02:34 +0800

    update setup

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+$BucketRepo = "https://github.com/hashcat26/bucket"
 $PackagesDir = "$PSScriptRoot\packages"
-$GlobalsDir = "$PSScriptRoot\packages\globals"
 Set-Location -LiteralPath $PSScriptRoot
 
 $ScoopFile = "downloads\scripts\scoop.ps1"
@@ -12,18 +12,18 @@ $PackageList = "gallery-dl", "spotdl", "yt-dlp"
 
 New-Item downloads\scripts, packages, utilities -ItemType Dir
 Invoke-RestMethod get.scoop.sh -OutFile $ScoopFile
-.$ScoopFile -ScoopDir $PackagesDir -ScoopGlobalDir $GlobalsDir
+.$ScoopFile -ScoopDir $PackagesDir
 
-$BucketRepo = "https://github.com/hashcat26/bucket"
+Invoke-Expression "scoop install git ; scoop bucket add extras"
 Invoke-Expression "scoop bucket add hashcat $BucketRepo"
-Invoke-Expression "scoop bucket add extras ; scoop status"
+Invoke-Expression "scoop uninstall git ; scoop status"
 
 ForEach ($App In $AppList) {
     Invoke-Expression "scoop install hashcat/$App"
 }
 
-$UpgradeCmd = "python -m pip install -U pip pipenv setuptools"
-Invoke-Expression "pip install pipenv ; $UpgradeCmd"
+Invoke-Expression "python -m pip install -U pip setuptools"
+Invoke-Expression "pip install pipenv ; pip install -U pipenv"
 Invoke-Expression "cd utilities ; md -f .venv | out-null"
 
 ForEach ($Package In $PackageList) {
