a29daadd7aee5a4ea2f0cf7f6a9ddbaa349193d9
hashcat26 <dowaitodoratore@gmail.com>
2023-08-04 22:35:36 +0800

    update setup

--- setup.ps1
+++ setup.ps1
@@ -14,15 +14,15 @@ New-Item downloads\scripts, packages, utilities -ItemType Dir
 Invoke-RestMethod get.scoop.sh -OutFile $ScoopFile
 .$ScoopFile -ScoopDir $PackagesDir
 
-Invoke-Expression "scoop install git ; scoop bucket add extras"
+Invoke-Expression "scoop install git ; scoop bucket rm main"
 Invoke-Expression "scoop bucket add hashcat $BucketRepo"
-Invoke-Expression "scoop uninstall git ; scoop status"
+Invoke-Expression "scoop uninstall 7zip git ; scoop status"
 
 ForEach ($App In $AppList) {
     Invoke-Expression "scoop install hashcat/$App"
 }
 
-Invoke-Expression "python -m pip install -U pip setuptools"
+Invoke-Expression "pip install -U pip setuptools"
 Invoke-Expression "pip install pipenv ; pip install -U pipenv"
 Invoke-Expression "cd utilities ; md -f .venv | out-null"
 
