0f9c0df98c9d9d02d82541e6f925aa292bd4865e
hashcat26 <dowaitodoratore@gmail.com>
2024-03-25 21:13:05 +0800

    update scripts

--- update.ps1
+++ update.ps1
@@ -30,6 +30,6 @@ Invoke-Expression "cp -r -force workspace/* ."
 Invoke-Expression "rm -r -force workspace ; attrib +h .git"
 Invoke-Expression "git remote update ; git pull"
 
-ForEach ($Cache In @(Get-ChildItem packages\cache)) {
-    Invoke-Expression "del -force $Cache"
+ForEach ($Cache In @(Get-ChildItem packages\cache).BaseName) {
+    Invoke-Expression "del -force packages/cache/$Cache"
 }
